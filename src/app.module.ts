3f2003987db97ecab1efd2b3b53558a51ff361cb
raven-panda <ambroise.maire7@gmail.com>
2025-03-04 13:19:05 +0100

    add: added database connection, removed app unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://apiUser:ApiUserSecret87!@localhost:27017/ebrarydb',
+    ),
+  ],
 })
 export class AppModule {}
