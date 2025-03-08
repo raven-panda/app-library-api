53f10462d267bf71a1e10e760e8cba235ebeabdf
raven-panda <ambroise.maire7@gmail.com>
2025-03-08 12:37:30 +0100

    add: update e2e book test

--- test/book.e2e-test.ts
+++ test/book.e2e-test.ts
@@ -1,10 +1,12 @@
 import * as request from 'supertest';
-import { BookDto } from '../src/app/book/dto/book.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app/app.module';
 import { BookModule } from '../src/app/book/book.module';
-import { INestApplication } from '@nestjs/common';
+import {INestApplication, ValidationPipe} from '@nestjs/common';
 import { App } from 'supertest/types';
+import { CreateBookDto } from '../src/app/book/dto/create-book.dto';
+import { BookDto } from '../src/app/book/dto/book.dto';
+import { isGivenFormatPhysical } from '../src/app/utils/book.utils';
 
 describe('When I request book resource', () => {
   let app: INestApplication<App>;
@@ -15,6 +17,11 @@ describe('When I request book resource', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -31,15 +38,15 @@ describe('When I request book resource', () => {
       });
   });
 
-  it('Then [POST]/book with valid body should return 201', () => {
-    const book: BookDto = {
+  it('Then [POST]/book with valid body should return 201 and data returned should be valid', () => {
+    const bookToCreate: CreateBookDto = {
       isbn: 'TestIsbn',
       title: 'TestTitle',
-      coverFileId: 'TestAuthor',
       editor: 'TestEditor',
+      coverFileId: 'test-file.jpeg',
       genre: 'NOVEL',
-      format: 'PDF',
-      isForRent: false,
+      format: 'PAPERBACK',
+      isForRent: true,
       isPhysicalFormat: false,
       languageCode: 'en',
       price: 0,
@@ -48,6 +55,41 @@ describe('When I request book resource', () => {
       description: 'Test long description',
     };
 
-    return request(app.getHttpServer()).post('/book').send(book).expect(201);
+    bookToCreate.isPhysicalFormat = isGivenFormatPhysical(bookToCreate.format);
+
+    return request(app.getHttpServer())
+      .post('/book')
+      .send(bookToCreate)
+      .expect(201)
+      .expect((res) => {
+        console.log(res.body);
+        expect(res.body).toBeTruthy();
+        const resDto: BookDto = res.body as BookDto;
+
+        expect(resDto.languageCode).toEqual(bookToCreate.languageCode);
+        expect(resDto.targetAudience).toEqual(bookToCreate.targetAudience);
+        expect(resDto.format).toEqual(bookToCreate.format);
+        expect(resDto.genre).toEqual(bookToCreate.genre);
+        expect(resDto.theme).toEqual(bookToCreate.theme);
+        expect(resDto.description).toEqual(bookToCreate.description);
+        expect(resDto.editor).toEqual(bookToCreate.editor);
+        expect(resDto.title).toEqual(bookToCreate.title);
+        expect(resDto.isbn).toEqual(bookToCreate.isbn);
+        expect(resDto.price).toEqual(bookToCreate.price);
+        expect(resDto.isPhysicalFormat).toEqual(bookToCreate.isPhysicalFormat);
+        expect(resDto.isForRent).toEqual(bookToCreate.isForRent);
+        expect(resDto.coverFileId).toEqual(bookToCreate.coverFileId);
+
+        expect(resDto.id).toBeTruthy();
+        expect(resDto.reviews).toEqual(0);
+        expect(resDto.averageRate).toEqual(0);
+      });
+  });
+
+  it('Then [POST]/book with invalid body should return 400', () => {
+    return request(app.getHttpServer())
+      .post('/book')
+      .send({})
+      .expect(400);
   });
 });
